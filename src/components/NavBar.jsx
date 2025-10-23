967938d8a1bf3f29e7f5ac82db45b166cf7aea76
Verah Mokaya <verahmokaya475@gmail.com>
2025-10-23 11:46:56 +0300

    added portfolio and Learn to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,21 +7,24 @@ function NavBar({ onToggleTheme, theme}) {
     <nav className="navbar">
         <div className="nav-left">
             <img src={logo} alt="CryptoVibe Logo" className="nav-logo" />
-            <h2 className="logo-text">CryptoVibe</h2>
+            <h2 className="logo-text">TrendyCryptoVibe</h2>
         </div>
 
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to=
         "/coins">Coins</Link></li>
-        <li><Link to="/add">Add Coin</Link></li>
-        <li><Link>Trending</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="learn">Learn</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
+        <div className="flex">
+        
+            <button className="theme-toggle" onClick={onToggleTheme}>
+                {theme === "dark" ? "Light" : "Dark"}
+            </button>
+        </div> 
 
-      <button className="theme-toggle" onClick={onToggleTheme}>
-        {theme === "dark" ? "Light" : "Dark"}
-      </button>
     </nav>
   );
 }
